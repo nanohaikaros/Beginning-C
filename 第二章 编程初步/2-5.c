566773b4f93f2be2748e5f5a0ac5bd52665b23f4
Nanoha <1371689491@qq.com>
2018-08-22 21:19:07 +0800

    2

---
+++ 第二章 编程初步/2-5.c
@@ -0,0 +1,21 @@
+// Program 2.5 Calculations with cookies
+#include <stdio.h>
+
+int main(void)
+{
+    int cookies = 5;
+    int cookie_calories = 125;          // Calories per cookie
+    int total_taten = 0;                // Total cookies eaten
+
+    int eaten = 2;                      // Number to be eaten
+    cookies = cookies - eaten;          // Subtract number eaten from cookies
+    total_taten = total_taten + eaten;
+    printf("\nI have eaten %d cookies. There are %d cookies left", eaten, cookies);
+
+    eaten = 3;                          // New value for cookies eaten
+    cookies = cookies - eaten;          // Subreact number eaten from cookies
+    total_taten = total_taten + eaten;
+    printf("\nI have eaten %d more. Now there are %d cookies left\n", eaten, cookies);
+    printf("\nTotal energy consumed is %d calories.\n", total_taten*cookie_calories);
+    return 0;
+}
