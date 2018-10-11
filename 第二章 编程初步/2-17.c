4d02493fc2c7ea1a93379eeb78e8c0def4cc2047
nanohaikaros <1371689491@qq.com>
2018-10-11 21:35:13 +0800

    2

---
+++ 第二章 编程初步/2-17.c
@@ -0,0 +1,45 @@
+// Program 2.17 Calculating the height of a tree
+#include <stdio.h>
+
+int main(void)
+{
+    long shorty = 0L;               // Shorty's height in inches
+    long lofty = 0L;                // Lofty's height in inches
+    long feet = 0L;
+    long inches = 0L;
+    long shorty_to_lofty = 0L;      // Distance from Shorty to Lofty in inches
+    long lofty_to_tree = 0L;        // Distance from Lofty to tree in inches
+    const long inches_per_foot = 12L;
+
+    // Get Lofty's height
+    printf("Enter Lofty's height to the top of his/her head, in whole feet: ");
+    scanf("%ld", &feet);
+    printf("                                            ...and then inches: ");
+    scanf("%ld", &inches);
+    lofty = feet*inches_per_foot + inches;
+
+    // Get Shorty's height up tp his/her eyes
+    printf("Enter Shorty's height up to his/her eyes, in whole feet: ");
+    scanf("%ld", &feet);
+    printf("                                     ...and then inches: ");
+    scanf("%ld", &inches);
+    shorty = feet*inches_per_foot + inches;
+
+    // Get the distance from Shorty to Lofty
+    printf("Enter the distance between Shorty and Lofty, in whole feet: ");
+    scanf("%ld", &feet);
+    printf("                                        ...and then inches: ");
+    scanf("%ld", &inches);
+    shorty_to_lofty = feet*inches_per_foot + inches;
+
+    // Get the distance from Lofty to the tree
+    printf("Finally enter the distance from Lofty to the tree to the nearest foot: ");
+    scanf("%ld", &feet);
+    lofty_to_tree = feet*inches_per_foot;
+
+    // The code to calulate the height of the tree will go here
+
+    // The code to display the result will go here
+
+    return 0;
+}
