a251bc8212a20ccfd57a53389324623e704ab04b
Nanoha <1371689491@qq.com>
2018-11-15 21:50:11 +0800

    4

---
+++ 第四章 循环/4-5.c
@@ -0,0 +1,18 @@
+// Program 4.4 Summing integers backward
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned long long sum = 0LL;       // Stores the sum of the integers
+    unsigned int count = 0;             // The number of integers to be summed
+
+    //Read the number of integers to be summed
+    printf("\nEnter the number of integers you want to sum: ");
+    scanf(" %u", &count);
+
+    // Sum integers from 1 to count 
+    for (unsigned int i = count; i >= 1; sum += i--);
+
+    printf("\nTotal of the first %u number is %llu\n", count, sum);
+    return 0;
+}
