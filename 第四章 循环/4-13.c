9c78c61ff4fc52606e1e642df9e6474bec22002e
Nanoha <1371689491@qq.com>
2018-11-24 21:18:16 +0800

    4

--- 第四章 循环/4-13.c
+++ 第四章 循环/4-13.c
@@ -2,12 +2,18 @@
 #include <stdio.h>              // For input and output
 #include <ctype.h>              // For toupper() function
 #include <stdbool.h>            // For bool, true, false
+#include <stdlib.h>             // For rand() and srand()
+#include <time.h>               // For time() funcrion
 
 int main(void)
 {
     char another_game = 'Y';    // Records if another game is to be played
     const unsigned int DELAY = 1;   // Display period in seconds
     bool correct = true;            //  true for correct sequence, false otherwise
+    unsigned int tries = 0;         // Number of successful entries for sequence length
+    unsigned int digits = 0;        // Number of digits in a sequence
+    time_t seed = 0;                // Seed value for random number sequence
+    unsigned int number = 0;        // Stores an input digit
 
     /* More variable declarations for the program */
 
@@ -24,12 +30,42 @@ int main(void)
     // Game loop - one outer loop iteration is a complets game
     do 
     {
+        // Initialize game
         correct = true;         // Indicates correct sequence entered
+        tries = 0;              // Initialize count of successful tries
+        digits = 2;             // Initial length of digit sequence
         // Other code to initialize the game
         // Inner loop continues as long as sequences are entered correctly
         while(correct)
         {
-            // Statements to play the game
+            ++tries;        //  A new attempt
+
+            // Generate a sequence of digits and display them
+            srand(time(&seed)); // Initialize the random sequence
+            for(unsigned int i = 1; i<=digits; ++i)
+                printf("%d ", rand()%10);   // Output a random digit
+
+                // Code to wait one second
+
+                // Code to overwrite the digit sequence
+
+                // Code to prompt for the input sequence
+
+                srand(seed);        // Reinitialize the random sequence
+                for(unsigned int i = 1; i<=digits; ++i)     
+                // Read the input sequance & check against the original
+                {
+                    scanf("%u", &number);       // Read a digit
+                    if(number != rand()%10)     // Compare with generated digit
+                    {
+                        correct = false;        // incorrect entry
+                        break;                  // No need to check fureher...
+                    }
+                }
+                // On every thied successful try, increase the sequence length
+                if(correct && ((tries % 3) == 0))
+                    ++digits;
+            printf("%s\n", correct ? "Correct!" : "Wrong!");
         }
 
         // Output the score when a game is finished
