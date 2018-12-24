f0c18f31152b488df867468ca048da88bce62e1d
Nanoha <1371689491@qq.com>
2018-12-24 13:05:13 +0800

    6

--- 第六章 字符串和文本的应用/6-10.c
+++ 第六章 字符串和文本的应用/6-10.c
@@ -39,9 +39,60 @@ int main(void)
         }
     }
 
-    // the code to find the words in the text array...
+    // find the first word
+    size_t len = TEXT_LEN;
+    char *ptr = NULL;
+    char* pWord = strtok_s(text, &len, delimiters, &ptr);   // find 1st word
 
-    // the code to output the words...
+    if(!pWord)
+    {
+        printf("No words found. Ending program.\n");
+        return 1;
+    }
+    strcpy_s(words[word_count], WORD_LEN, pWord);
+    ++nword[word_count++];
+
+    // find the rest of the words
+    bool new_word = true;       // false for an existing word
+    while(true)
+    {
+        pWord = strtok_s(NULL, &len, delimiters, &ptr); // find subsequent word
+        if(!pWord)
+            break;          // NULL ends tokenizing
+        
+        // check for existing word
+        for(int i=0; i<word_count; ++i)
+        {
+            if(strcmp(words[i], pWord) == 0)
+            {
+                ++nword[i];
+                new_word = false;
+            }
+        }
+
+        if(new_word)                // ture if new word
+        {
+            strcpy_s(words[word_count], WORD_LEN, pWord);   // copy to array
+            ++nword[word_count++];      // increment count and index
+        }
+        else
+            new_word = true;            // reset new word flag
+
+        if(word_count>MAX_WORDS-1)
+        {
+            printf("Capacity to store words exceeded.\n");
+            return 1;
+        }
+    }
+
+    // list the words
+    for(int i=0; i<word_count; ++i)
+    {
+        printf(" %-13s %3d", words[i], nword[i]);
+        if((i+1)%4 == 0)
+            printf("\n");
+    }
+    printf("\n");
 
     return 0;
 }
