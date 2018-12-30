088fa77132a306005d589edf908953982c27f66b
Nanoha <1371689491@qq.com>
2018-12-30 14:01:55 +0800

    7

---
+++ 第七章 指针/7-4.c
@@ -0,0 +1,15 @@
+// Program 7.4 Arrays and pointer
+#include <stdio.h>
+
+int main(void)
+{
+    char multiple[] = "My string";
+
+    char *p = &multiple[0];
+
+    printf("The address of the first array element: %p\n", p);
+
+    p = multiple;
+    printf("The address obtained from the array name: %p\n", multiple);
+    return 0;
+}
