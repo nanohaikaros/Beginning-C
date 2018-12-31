0c7e8293109304f78d7330fd42ccc5d5e0d89719
Nanoha <1371689491@qq.com>
2018-12-31 19:11:00 +0800

    7

---
+++ 第七章 指针/7-9.c
@@ -0,0 +1,16 @@
+// Program 7.9 Multdimensional arrays and pointers
+#include <stdio.h>
+
+int main(void)
+{
+    char board[3][3] = {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'}
+    };
+
+    char *pboard = *board;      // a pointer to char
+    for(int i=0; i<9; ++i)
+        printf("board: %c\n", *(pboard+i));
+        return 0;
+}
